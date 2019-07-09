5cd41c1d1f61324532ef3f20ef47ea2f80b1148a
Matthew Guzman <tomaffew@me.com>
2019-07-09 04:59:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,9 @@
 # Your Code Here
 
-
-
-
-
+def map(source_array)
+ new_array = []
+   source_array.length.times do |index|
+     new_array.push(yield(source_array[index]))
+   end
+ return new_array
+end
