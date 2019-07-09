47c34238d22f7a399b91738338e1e79bb93785e8
Matthew Guzman <tomaffew@me.com>
2019-07-09 05:00:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,16 @@ def map(source_array)
    end
  return new_array
 end
+
+def reduce(source_array, starting_point = 0)
+  running_total = starting_point
+    source_array.length.times do |index|
+      running_total = yield(source_array[index], running_total)
+      print running_total
+    end
+  
+    if(running_total == nil)
+      running_total = false
+    end
+  return running_total
+end
